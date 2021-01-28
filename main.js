37d23945a1839f5e2db84ed93959aee849df5332
ctalloc <cecilia.tallo@gmail.com>
2021-01-28 09:59:43 +0100

    Created classes , random number generator and else if function.

---
+++ main.js
@@ -0,0 +1,37 @@
+"use strict";
+
+const tryBtnElement = document.querySelector('.js-trybtn');
+const tryNumberElement = document.querySelector('.js-trynumber');
+const clueElement = document.querySelector('.js-clue');
+const randomNumber = Math.ceil(Math.random() * 100);
+console.log(`El número aleatorio es ${randomNumber}`)
+
+/* function getRandomNumber() {
+  const randomNumber = Math.ceil(Math.random() * 100);
+  console.log(`El número aleatorio es ${randomNumber}`);
+}; */
+
+
+function handleTryBtn() {
+    const tryNumberValue =  parseInt(tryNumberElement.value);
+    console.log(tryNumberValue);
+    if (tryNumberValue <= 0) {
+        clueElement.innerHTML = '¿Seguro que has leído bien las instrucciones? Ese número no es válido.';
+    }
+    else if (tryNumberValue >= 100) {
+        clueElement.innerHTML = '¿Seguro que has leído bien las instrucciones? Ese número no es válido.';
+    }
+    else if (tryNumberValue < randomNumber) {
+        clueElement.innerHTML = 'Te has quedado corta, échale más.';
+    }
+    else if (tryNumberValue > randomNumber) {
+        clueElement.innerHTML = 'Te has pasado, quítale un poco.';
+    }
+    else if (tryNumberValue == randomNumber) {
+        clueElement.innerHTML = 'Lo has adivinado! Minipunto para ti!';
+    }
+};
+
+tryBtnElement.addEventListener('click', handleTryBtn);
+
+
