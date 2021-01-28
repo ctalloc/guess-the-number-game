0b8bd0b6006148eafede58d5f2850a2fe0df49fc
ctalloc <cecilia.tallo@gmail.com>
2021-01-28 10:11:51 +0100

    Add try counter to game in js.

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 const tryBtnElement = document.querySelector('.js-trybtn');
 const tryNumberElement = document.querySelector('.js-trynumber');
 const clueElement = document.querySelector('.js-clue');
+const tryCounter = document.querySelector('.js-tryCounter');
 const randomNumber = Math.ceil(Math.random() * 100);
 console.log(`El número aleatorio es ${randomNumber}`)
 
@@ -27,9 +28,10 @@ function handleTryBtn() {
     else if (tryNumberValue > randomNumber) {
         clueElement.innerHTML = 'Te has pasado, quítale un poco.';
     }
-    else if (tryNumberValue == randomNumber) {
+    else if (tryNumberValue === randomNumber) {
         clueElement.innerHTML = 'Lo has adivinado! Minipunto para ti!';
-    }
+    };
+    tryCounter.innerHTML = `${parseInt(tryCounter.innerHTML) +1}`;
 };
 
 tryBtnElement.addEventListener('click', handleTryBtn);
